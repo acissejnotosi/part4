9e30970b7b597fb595b05ea82ec67e38ba4ce2b0
Jessica Isoton <jessicasampaio195@gmail.com>
2020-06-05 14:12:59 +0200

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  let total = blogs.map((blog) => blog.likes);
+  if (total.length > 0) {
+    return total.reduce(reducer);
+  }
+  return 0;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
