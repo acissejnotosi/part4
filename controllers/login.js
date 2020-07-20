25d3fedba578c16e50964caffae2b601303d4443
Jessica Isoton <jessicasampaio195@gmail.com>
2020-07-20 15:04:06 +0200

    5.20: bloglist end to end testing, step4

--- controllers/login.js
+++ controllers/login.js
@@ -6,11 +6,13 @@ const User = require("../models/user");
 loginRouter.post("/", async (request, response) => {
   const body = request.body;
   const user = await User.findOne({ username: body.username });
+ 
   const passwordCorrect =
     user === null
       ? false
       : await bcrypt.compare(body.password, user.passwordHash);
 
+    console.log(passwordCorrect);
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
       error: "invalid username or password",
